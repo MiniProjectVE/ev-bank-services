6595e60fb0b9a3b03c446393d045a2ac6aad33e7
vincent207 <54823885+vincent207@users.noreply.github.com>
2023-04-18 10:48:28 +0700

    Initial upload

---
+++ ev-bank-services/src/main/java/id/co/bca/spring/evbankservices/EvBankServicesApplication.java
@@ -0,0 +1,13 @@
+package id.co.bca.spring.evbankservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvBankServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvBankServicesApplication.class, args);
+	}
+
+}
